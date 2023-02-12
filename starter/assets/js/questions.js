05ea258132a5451b46b6735d32b85bf3368a43ee
steelsnake93 <nikolanikotasev@protonmail.com>
2023-02-12 13:37:30 +0100

    Updated questions and answers

---
+++ starter/assets/js/questions.js
@@ -0,0 +1,30 @@
+const questions =
+[
+    {
+        "question": "1. What sort of animal is Walt Disney's Dumbo?",
+        "answers": ["A. Deer", "B. Rabbit", "C. Elephant", "D. Donkey"],
+        "correct": 2
+    },
+    {
+        "question": "2. What was the name of the Spanish waiter in the TV sitcom 'Fawlty Towers'",
+        "answers": ["A. Manuel", "B. Pedro", "C. Alfonso", "D. Javier"],
+        "correct": 0
+    },
+    {
+        "question": "3. Which battles took place between the Royal Houses of York and Lancaster?",
+        "answers": ["A. Thirty Years War", "B. Hundred Years War", "C. War of the Roses", "D. English Civil War"],
+        "correct": 2
+    },
+    
+    {
+        "question": "4. Which former Beatle narrated the TV adventures of Thomas the Tank Engine?",
+        "answers": ["A. John Lennon", "B. Paul McCartney", "C. George Harrison", "D. Ringo Starr"],
+        "correct": 3
+    },
+    
+    {
+        "question": "5. Queen Anne was the daughter of which English Monarch?",
+        "answers": ["A. James II", "B. Henry VIII", "C. Victoria", "D. William I"],
+        "correct": 0
+    }
+]
